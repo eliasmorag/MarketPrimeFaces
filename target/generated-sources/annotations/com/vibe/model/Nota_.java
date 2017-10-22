5b8719584f4abd49285df31a20e760ffef11a1cb
matirivas1995 <matias.rt@hotmail.com>
2017-10-22 17:32:02 -0300

    se puede exportar badddbunnbebebeebebeeb

--- target/generated-sources/annotations/com/vibe/model/Nota_.java
+++ target/generated-sources/annotations/com/vibe/model/Nota_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-21T18:57:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-22T17:18:53")
 @StaticMetamodel(Nota.class)
 public class Nota_ { 
 
